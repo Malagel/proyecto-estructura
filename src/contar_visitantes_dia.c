1a38cd31fcaa860684e469d952592b17165d707c
Manolo580 <manuelbaezzuniga06@gmail.com>
2026-05-26 16:40:09 -0400

    Funcion terminada, verifica primero que la fecha_buscada y la fecha_ingreso sean la misma y ahi suma al cont (contador) y despues suma los visitantes de la derecha del arbol y izquierda

--- src/contar_visitantes_dia.c
+++ src/contar_visitantes_dia.c
@@ -7,11 +7,11 @@ int contar_visitantes_dia(struct NodoVisitantes *visitantes, char *fecha_buscada
 
     int cont = 0;
 
-    if(stcrmp(visitantes->v->entrada->fecha_ingreso, fecha_buscada) == 0){
+    if(strcmp(visitantes->v->entrada->fecha_ingreso, fecha_buscada) == 0){
         cont ++; /*Sumamos ya que si entro el dia que queremos */
     }
 
 
     /*Aqui sumamos lo que se encuentra en el lado izquierdo del arbol y derecho, con el nodo principal. Y return al valor */
-    return contador + contar_visitantes_dia(visitantes->izq, fecha_buscada) + contar_visitantes_dia(visitantes->der, fecha_buscada);
+    return cont + contar_visitantes_dia(visitantes->izq, fecha_buscada) + contar_visitantes_dia(visitantes->der, fecha_buscada);
 };
