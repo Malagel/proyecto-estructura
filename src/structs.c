fc8dfdff14f76b380c147d0224938c999343013e
Nicolás Mariángel <cubemasterr@gmail.com>
2026-05-26 18:19:59 -0400

    SE CAMBIÓ TODO

--- src/structs.c
+++ src/structs.c
@@ -1,34 +1,47 @@
 struct Parque {
     int cap_max;
     struct NodoVisitantes *raiz_visitantes; /* árbol binario ordenado por rut */
+    struct NodoZonas *head_zonas; /* Simplemente enlazada */
 };
 
 struct NodoVisitantes {
-    struct Visitante *v;
+    struct Visitante *datos;
     struct NodoVisitantes *izq, *der;
 };
 
 struct Visitante {
     char *nombre;
-    char *rut;
-    char *tipo; 
-    struct Entradas *entrada;
+    char rut[12];
+    int edad;
+    float altura;
+
+    struct Entrada *entrada;
 };
 
-struct Entradas {
-    int codigo;
+struct Entrada {
+    int id;
     char *tipo;
     int valor;
-    char *estado;
-    char *fecha_ingreso;
+    char *estado; /* ["activa", "utilizada", "vencida", "anulada"] */
+    char fecha_ingreso[11]; /* Formato: DD-MM-YYYY */
+};
+
+struct NodoZonas {
+    struct Zona *datos;
+    struct NodoZonas *sig;
 };
 
 struct Zona {
-    int codigo;
+    int id;
     char *nombre;
-    char *tematica;
-    struct Tiempo *hora_apertura;
-    struct Tiempo *hora_cierre;
+    char *tematica; 
+
+    int cap_max;
+    int visitantes_actuales;
+    
+    struct Tiempo hora_apertura;
+    struct Tiempo hora_cierre;
+
     int atracciones_max;
     struct NodoAtraccion *head_atracciones;
 };
@@ -43,18 +56,28 @@ struct NodoAtraccion {
     struct NodoAtraccion *sig;
 };
 
-struct Atracción {
+struct Atraccion {
     char *nombre;
-    int capacidad;
-    int duracion;
-    float altura;
-    int edad;
-    struct Fila *cola_pobres;
-    struct Fila *cola_ricos;
+    char *estado; /* ["operativa", "mantenimiento", "cerrada", "fuera_de_servicio"] */
+    char *tematica;
+    int duracion; /* En minutos */
+
+    int edad_min;
+    float altura_min;
+
+    struct Fila cola_general;
+    struct Fila cola_prioritaria;
+
+    int cap_max; /* Capacidad por ciclo */
+    int visitantes_totales;
+
+    int max_cola_general;
+    int max_cola_prioritaria;
 };
 
 struct NodoFila {
-    int id_visitante;
+    int ids_grupo[10]; /* ids_grupo[0] es el representante del grupo, la id de su entrada */
+    int tam_grupo;
     struct NodoFila *sig;
 };
 
