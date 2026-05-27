9899323171a9a51f8a6d887400cef5f343cbefab
Manolo580 <manuelbaezzuniga06@gmail.com>
2026-05-27 12:50:37 -0400

    Hice la funcion para ver cuantas personas estan dentro del parque con la entrada, muy similar a la funcion anterior

--- src/visitantes.c
+++ src/visitantes.c
@@ -17,3 +17,18 @@ int contar_visitantes_dia(struct NodoVisitantes *visitantes, char *fecha_buscada
     /*Aqui sumamos lo que se encuentra en el lado izquierdo del arbol y derecho, con el nodo principal. Y return al valor */
     return cont + contar_visitantes_dia(visitantes->izq, fecha_buscada) + contar_visitantes_dia(visitantes->der, fecha_buscada);
 };
+
+int personas_dentro_parque(struct NodoVisitantes *visitantes) {
+    if (visitantes == NULL || visitantes->datos == NULL) {
+        return 0;
+    }
+
+    int cont = 0;
+
+    if (strcmp(visitantes->datos->entrada->estado, "utilizada") == 0) {
+        cont++; /*Sumamos ya que la entrada es utilizada*/
+    }
+
+    /*Ahora la misma suma que en la función anterior*/
+    return cont + personas_dentro_parque(visitantes->izq) + personas_dentro_parque(visitantes->der);
+};
