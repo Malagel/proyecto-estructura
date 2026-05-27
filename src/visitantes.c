61a9ed0b493810a7198c9961cb805ef1c3c66709
Manolo580 <manuelbaezzuniga06@gmail.com>
2026-05-27 14:59:47 -0400

    Arregle la flecha y elimine algunos espacios y comentarios

--- src/visitantes.c
+++ src/visitantes.c
@@ -1,7 +1,5 @@
 #include <string.h>
 
-
-/*Funcion para contar los visitantes de todo el dia*/
 int contar_visitantes_dia(struct NodoVisitantes *visitantes, char *fecha_buscada){
     if(visitantes == NULL || visitantes->datos == NULL){
         return 0;
@@ -9,10 +7,9 @@ int contar_visitantes_dia(struct NodoVisitantes *visitantes, char *fecha_buscada
 
     int cont = 0;
 
-    if(strcmp(visitantes-datos->entrada->fecha_ingreso, fecha_buscada) == 0){
+    if(strcmp(visitantes->datos->entrada->fecha_ingreso, fecha_buscada) == 0){
         cont ++; /*Sumamos ya que si entro el dia que queremos */
     }
-
 
     /*Aqui sumamos lo que se encuentra en el lado izquierdo del arbol y derecho, con el nodo principal. Y return al valor */
     return cont + contar_visitantes_dia(visitantes->izq, fecha_buscada) + contar_visitantes_dia(visitantes->der, fecha_buscada);
@@ -32,11 +29,9 @@ int personas_dentro_parque(struct NodoVisitantes *visitantes, char *fecha_buscad
         }
     }
 
-    /*Ahora la misma suma que en la función anterior*/
     return cont + personas_dentro_parque(visitantes->izq) + personas_dentro_parque(visitantes->der);
 };
 
-/*Funcion para ver cuanto falta para llegar a la capacidad maxima del parque*/
 int falta_para_cap_max(struct Parque *parque){
     if(parque->raiz_visitantes == NULL){
         return parque->cap_max;
@@ -44,6 +39,6 @@ int falta_para_cap_max(struct Parque *parque){
 
     int personas_dentro = personas_dentro_parque(parque->NodoVisitantes);
 
-    return parque->cap_max - personas_dentro; 
     /*Devolvemos la diferencia entre la capacidad maxima y las personas que estan adentro*/
+    return parque->cap_max - personas_dentro; 
 }
