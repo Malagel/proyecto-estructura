bdf88fc862cee3842293c595a89cf419c056df5a
Manolo580 <manuelbaezzuniga06@gmail.com>
2026-05-27 12:32:40 -0400

    Cree el archivo y puse la funcion para contar los visitantes del dia

---
+++ src/visitantes.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+
+/*Funcion para contar los visitantes de todo el dia*/
+int contar_visitantes_dia(struct NodoVisitantes *visitantes, char *fecha_buscada){
+    if(visitantes == NULL || visitantes->v == NULL){
+        return 0;
+    }
+
+    int cont = 0;
+
+    if(strcmp(visitantes->v->entrada->fecha_ingreso, fecha_buscada) == 0){
+        cont ++; /*Sumamos ya que si entro el dia que queremos */
+    }
+
+
+    /*Aqui sumamos lo que se encuentra en el lado izquierdo del arbol y derecho, con el nodo principal. Y return al valor */
+    return cont + contar_visitantes_dia(visitantes->izq, fecha_buscada) + contar_visitantes_dia(visitantes->der, fecha_buscada);
+};
