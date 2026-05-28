cca0035ab84b41f7dd55b353d0c0b8369c2ee9ca
Manolo580 <manuelbaezzuniga06@gmail.com>
2026-05-28 17:05:42 -0400

    Ahi cambie y elimine todo lo que se ocupa con fecha y hice que si la entrada fuera vencida en la funcion de personas dentro parque, descuente.

--- src/visitantes.c
+++ src/visitantes.c
@@ -1,39 +1,41 @@
 #include <string.h>
 #include "structs.h"
 
-int contar_visitantes_dia(struct NodoVisitantes *visitantes, char* fecha_actual){
+int contar_visitantes_dia(struct NodoVisitantes *visitantes){
     if(visitantes == NULL || visitantes->datos == NULL){
         return 0;
     }
     int cont = 0;
 
-    if(strcmp(visitantes->datos->entrada->fecha_ingreso, fecha_actual) == 0){
-        cont ++; /*Sumamos ya que si entro el dia que queremos */
+    if (strcmp(visitantes->datos->entrada->estado, "utilizada") == 0) {
+        cont++; 
     }
     /*Aqui sumamos lo que se encuentra en el lado izquierdo del arbol y derecho, con el nodo principal. Y return al valor */
-    return cont + contar_visitantes_dia(visitantes->izq, fecha_actual) + contar_visitantes_dia(visitantes->der, fecha_actual);
+    return cont + contar_visitantes_dia(visitantes->izq) + contar_visitantes_dia(visitantes->der);
 }
 
 /*Funcion para ver cuantas personas estan dentro del parque*/
-int personas_dentro_parque(struct NodoVisitantes *visitantes, char *fecha_actual) {
+int personas_dentro_parque(struct NodoVisitantes *visitantes) {
     if (visitantes == NULL || visitantes->datos == NULL) {
         return 0;
     }
     int cont = 0;
 
     if (strcmp(visitantes->datos->entrada->estado, "utilizada") == 0) {
-        if(strcmp(visitantes->datos->entrada->fecha_ingreso, fecha_actual) == 0){
-            cont++; /*Sumamos ya que la entrada es utilizada*/
-        }
+        cont++; 
     }
-    return cont + personas_dentro_parque(visitantes->izq, fecha_actual) + personas_dentro_parque(visitantes->der, fecha_actual);
+
+    if (strcmp(visitantes->datos->entrada->estado, "vencida") == 0) {
+        cont--; 
+    }
+    return cont + personas_dentro_parque(visitantes->izq) + personas_dentro_parque(visitantes->der);
 }
 
-int falta_para_cap_max(struct Parque *parque, char *fecha_actual){
+int falta_para_cap_max(struct Parque *parque){
     if(parque->raiz_visitantes == NULL){
         return parque->cap_max;
     }
-    int personas_dentro = personas_dentro_parque(parque->raiz_visitantes, fecha_actual);
+    int personas_dentro = personas_dentro_parque(parque->raiz_visitantes);
 
     /*Devolvemos la diferencia entre la capacidad maxima y las personas que estan adentro*/
     return parque->cap_max - personas_dentro; 
