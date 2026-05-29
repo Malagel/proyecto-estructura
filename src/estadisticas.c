04e65e3f431180f65ffa0a7416ac65ab15fa77ee
Nicolás Mariángel <cubemasterr@gmail.com>
2026-05-28 21:04:53 -0400

    Creacion de estadisticas.c

---
+++ src/estadisticas.c
@@ -0,0 +1,8 @@
+#include "structs.h"
+#include "visitantes.h"
+
+funcion_reporte_general_dia(struct Parque *parque) {
+    int visitantes_totales, visitantes_dentro, visitantes_para_capacidad_maxima;
+
+    
+}
