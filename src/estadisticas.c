7da4043715cfbe600f4c8d2a3948c8422a1f295b
Nicolás Mariángel <cubemasterr@gmail.com>
2026-05-28 21:43:27 -0400

    Se creo base de función reporte general

--- src/estadisticas.c
+++ src/estadisticas.c
@@ -3,6 +3,9 @@
 
 funcion_reporte_general_dia(struct Parque *parque) {
     int visitantes_totales, visitantes_dentro, visitantes_para_capacidad_maxima;
-
     
+    visitantes_totales = contar_visitantes_dia(parque->raiz_visitantes);
+    visitantes_dentro = personas_dentro_parque(parque->raiz_visitantes);
+    visitantes_para_capacidad_maxima = falta_para_cap_max(parque);
+
 }
